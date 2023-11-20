fea471300fd66b9b93ecf10ae834f6397b7a09e9
Shreekrishna Ravishankara Bhat <sbhat97@gatech.edu>
2023-11-19 19:28:04 -0500

    Added flag support and max depth

--- ex5/main.go
+++ ex5/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"encoding/xml"
+	"flag"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -12,11 +14,19 @@ import (
 
 var visited map[string]bool = make(map[string]bool)
 
+type Url struct {
+	Loc string `xml:"loc"`
+}
+type urlset struct {
+	Xmlns string `xml:"xmlns,attr"`
+	Url   []Url  `xml:"url"`
+}
+
 // DFS function takes a string as the root node.
 // It assumes that the URL already contains the correct origin. It performs a GET request on the link, extracts all the urls in the page, and runs the DFS on those methods as well.
 // Using a map, it ensures that it doesen't visit the same link twice.
-func dfs(root string, origin string) {
-	if root == "" {
+func dfs(root string, origin string, maxDepth int) {
+	if root == "" || maxDepth < 0 {
 		return
 	}
 
@@ -63,14 +73,25 @@ func dfs(root string, origin string) {
 			continue
 		}
 
-		dfs(finalLink, origin)
+		dfs(finalLink, origin, maxDepth-1)
 	}
 }
 
 func main() {
 	startURL := "https://www.calhoun.io/"
-	dfs(startURL, startURL)
+	maxDepth := 3
+	flag.StringVar(&startURL, "startURL", startURL, "The origin URL to return the sitemap for.")
+	flag.IntVar(&maxDepth, "maxDepth", maxDepth, "The maximum depth to run the sitempap for.")
+	flag.Parse()
+	dfs(startURL, startURL, maxDepth)
+	sitemap := urlset{Xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9", Url: make([]Url, 0)}
 	for visitedURL := range visited {
-		fmt.Println(visitedURL)
+		sitemap.Url = append(sitemap.Url, Url{Loc: visitedURL})
 	}
+	data, err := xml.MarshalIndent(sitemap, "", " ")
+	if err != nil {
+		fmt.Println("Error when parsing marshalling.")
+		fmt.Println(err)
+	}
+	fmt.Println(xml.Header + string(data))
 }
